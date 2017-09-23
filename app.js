b38f1c187f45de309b57388651da7fcd7533a912
jorgenr17 <jorgenarvaezromero@gmail.com>
2017-09-23 14:27:45 -0500

    v4
    Jorge Narváez
    Páginas Dinámicas y mejor vista en el formulario.

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+Vue.component('plantilla',{
+    template:`
+    <div>
+    <div class="card mb-3">
+    <div class="card-header">
+    <H2 align="center">Acreditación</H2>
+      <div class="card-body">
+       <p>Esta es la conceptualización genereal de Acreditaión</p>
+      </div>
+       </div>
+       </div>`
+}),
+Vue.component('plantilla2',{
+    template:`
+    <div>
+    <div class="card mb-3">
+    <div class="card-header">
+    <H2 align="center">Acreditación en Informática</H2>
+      <div class="card-body">
+       <p>Esta es la conceptualización de Acreditaión en la Liceniatura en Informática y Medios Audiovisuales.</p>
+      </div>
+       </div>
+       </div>`
+})
+
 firebase.initializeApp(config);
 const dbejemplo = firebase.database();
 new Vue({
@@ -5,38 +30,65 @@ new Vue({
     created(){
        dbejemplo.ref('actividades/').on('value', snapshot => 
        this.leerDatos(snapshot.val()))
+       dbejemplo.ref('templates/').on('value', snapshot => 
+       this.leerTemplate(snapshot.val()))     
    },
     data:{
         ocultar : true,
+        ocultar2 : false,
+        ocultar3 : false,
         nuevaActividad:"",
         nuevoResponsable:"",
         nuevaDescripcion:"",
         nuevaTarea:"",
         datos: [],
-        index:"",
+        templates:[],
     },
 methods:{
-    mostrar: function(){
-       this.ocultar = !this.ocultar
+    mostrar1: function(){
+       if(this.ocultar2 = true, this.ocultar3 = true){
+        this.ocultar2 = !this.ocultar2
+        this.ocultar3 = !this.ocultar3
+        this.ocultar = true         
+        }
+     },
+    mostrar2: function(){
+    if(this.ocultar = true, this.ocultar3 = true){
+        this.ocultar = !this.ocultar
+        this.ocultar3 = !this.ocultar3
+        this.ocultar2 = true         
+        }
+     },
+    mostrar3: function(){
+    if(this.ocultar = true, this.ocultar2 = true){
+        this.ocultar = !this.ocultar
+        this.ocultar2 = !this.ocultar2
+        this.ocultar3 = true         
+        }
      },
     agregarActividad: function(actividad, responsable, descripcion, tarea){
         dbejemplo.ref('actividades/').push({
         Actividad: this.nuevaActividad,
-        descripcion: this.nuevaDescripcion,
+        descripcion: this.nuevaDescripcion
     })
         dbejemplo.ref('responsables/').push({
         Responsable: this.nuevoResponsable,
-        Tareas:this.nuevaTarea,  
+        Tareas:this.nuevaTarea  
     })
 },
    leerDatos: function(datos){
         this.datos = [];
        for( let index in datos){
-           this.datos.push(datos[index])     
+           this.datos.push(datos[index])    
        }
    },
+    leerTemplate: function(templates){
+       for( let index in templates){
+           this.templates.push(templates[index])
+       }
+    },  
     eliminarNombre: function(index){
-        dbejemplo.ref('actividades/'+index).remove()
+        dbejemplo.ref('actividades/'+ index).remove()
         // this.actividades.splice(index, 1);
     },
     // editar: function(actividades){
@@ -44,6 +96,6 @@ methods:{
     //         Actividad: actividades.Actividad
     //     })
     // },
-    
+        
 }
 });
